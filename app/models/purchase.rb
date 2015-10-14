fb8f2211af234ba306644b6ab4f9afe15b77d008
seanreed1111 <seanreed1111@gmail.com>
2015-10-14 10:57:22 -0400

    adds uppercase to all names stored in model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,12 @@ class Purchase < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: {case_sensitive: false, message: ": An item with this name has already been entered into the database"}
   validates :cost, numericality: {greater_than: 0}
+
+  before_validation :uppercase_name
+
+  def uppercase_name
+    self.name.upcase!
+  end
+
+ 
 end
